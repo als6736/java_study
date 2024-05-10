2502706fed793243d4662bb6ff3d8c80d5d085dc
JongMin Park <als6736@gmail.com>
2024-05-10 14:11:09 +0900

    school_0510

--- src/Main.java
+++ src/Main.java
@@ -4,23 +4,14 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String A = sc.nextLine();
-        A = A.toLowerCase();
-        int arr[] = new int[26];
-        char ch = '?';
-        int max = 0;
-        for (int i =0; i<A.length();i++){
-            arr[A.charAt(i)-'a']++;
-        }
-        for (int i =0; i<26;i++){
-            if(arr[i]>max){
-                max = arr[i];
-                ch = (char)( i +'A');
+        int len = A.length();
+        int count = 0;
+        for (int i= 0; i<len;i++){
+            if(i<len-2){
+                
             }
-            else if (arr[i] == max){
-                ch = '?';
-            }
+            
         }
-        System.out.println(ch);
         sc.close();
     }
 }
